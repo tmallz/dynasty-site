c63d5a9461229ce1581c06146d1dc224a39186e7
Timothy O'Malley <tomalley24@gmail.com>
2025-03-28 17:43:27 -0500

    finish constitution

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -8,106 +8,201 @@
 			<h2 class="mb-4 text-center text-2xl font-semibold">Table of Contents</h2>
 			<ul class="list-inside list-disc">
 				<li><a href="#roster" class="hover:text-primary">Section 1: Roster</a></li>
-				<li><a href="#drafts" class="hover:text-primary">Section 2: Drafting</a></li>
+				<li><a href="#drafting" class="hover:text-primary">Section 2: Drafting</a></li>
 				<li><a href="#scoring" class="hover:text-primary">Section 3: Scoring System</a></li>
-				<li><a href="#playoffs" class="hover:text-primary">Section 4: Postseason</a></li>
-				<li><a href="#section-5" class="hover:text-primary">Section 5: Tanking Policy</a></li>
-				<li><a href="#section-6" class="hover:text-primary">Section 6: Replacing Managers</a></li>
-				<li><a href="#section-7" class="hover:text-primary">Section 7: League Finances</a></li>
+				<li><a href="#Postseason" class="hover:text-primary">Section 4: Postseason</a></li>
+				<li><a href="#tanking" class="hover:text-primary">Section 5: Tanking Policy</a></li>
+				<li><a href="#finances" class="hover:text-primary">Section 6: Finances</a></li>
+				<li><a href="#rules" class="hover:text-primary">Section 7: League Rules</a></li>
 			</ul>
 		</div>
 
 		<!-- Section 1 -->
 		<section id="roster" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 1: Roster</h2>
+			<h2 class="mb-4 text-3xl font-bold underline">Section 1: Roster</h2>
+
+			<h3 class="mt-4 text-xl font-semibold underline">1.1 Positional Breakdown</h3>
 			<ul class="list-inside list-disc space-y-2">
-				<li>1.1 Positional Breakdown</li>
-				<li>1.1.1 Position Maximums</li>
-				<li>1.2 Trading</li>
-				<li>1.2.1 Trade Collusion</li>
-				<li>1.2.2 Lending Players Prohibition</li>
-				<li>1.2.3 Trade Deadline</li>
-				<li>1.3 Waiver Wire</li>
+				<li>25 Total active Players, No position maximums</li>
 			</ul>
-			<p class="mt-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+
+			<h4 class="mt-4 text-lg font-semibold underline">Starters</h4>
+			<ul class="list-inside list-disc space-y-2">
+				<li>1 QB</li>
+				<li>2 RB</li>
+				<li>3 WR</li>
+				<li>1 TE</li>
+				<li>2 FLEX (RB/WR/TE)</li>
+				<li>1 FLEX (QB/RB/WR/TE)</li>
+			</ul>
+
+			<h4 class="mt-4 text-lg font-semibold underline">Bench</h4>
+			<ul class="list-inside list-disc space-y-2">
+				<li>15 Bench Spots</li>
+			</ul>
+
+			<h4 class="mt-4 text-lg font-semibold underline">Injured Reserve</h4>
+			<ul class="list-inside list-disc space-y-2">
+				<li>4 Injured Reserve Spots (Players must have sleeper designation of IR or Out)</li>
+			</ul>
+
+			<h4 class="mt-4 text-lg font-semibold underline">Taxi Squad</h4>
+			<ul class="list-inside list-disc space-y-2">
+				<li>4 Taxi Squad Spots (Only Rookies can be placed on Taxi Squad)</li>
+			</ul>
+
+			<h3 class="mt-4 text-xl font-semibold underline">1.2 Trades</h3>
+			<p class="mt-2">
+				Trading of all players and draft picks (up to 3 drafts away) is allowed. Trades will be
+				processed ASAP after they are accepted. Trades can be vetoed by the commissioner if they are
+				deemed to be collusion. Dumb/Bad trades where one manager is 'fleeced' will be allowed as
+				long as there are not clear and obvious signs of collusion. Collusion will be deemed by the
+				commissioner and co-commissioner(s) and no one else. People found guilty of collusion will
+				be publicly shamed in the group chat and banned if there is a pattern of collusion.
+			</p>
+
+			<h4 class="mt-4 text-lg font-semibold underline">1.2.1 Trade Deadline</h4>
+			<p class="mt-2">
+				The trade deadline is the start of the first game of week 13. No trades will be allowed
+				after this time.
+			</p>
+
+			<h3 class="mt-4 text-xl font-semibold underline">1.3 Waivers</h3>
+			<p class="mt-2">
+				A FAAB waiver budget will be used for waivers. Each manager will have $1000 to spend, and no
+				sleeper generated waiver suggestions will be provided. Waivers will be processed at 2 A.M.
+				Wednesday morning every week. Players will be on waivers for 2 days after they are dropped.
 			</p>
 		</section>
 
 		<!-- Section 2 -->
-		<section id="drafts" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 2: Drafting</h2>
-			<ul class="list-inside list-disc space-y-2">
-				<li>2.1 Draft Order</li>
-				<li>2.2 Rookie Drafts</li>
-				<li>2.3 Draft Day Trades</li>
-			</ul>
-			<p class="mt-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+		<section id="drafting" class="mb-8">
+			<h2 class="mb-4 text-3xl font-bold underline">Section 2: Drafting</h2>
+
+			<h3 class="mt-4 text-xl font-semibold underline">2.1 Rookie Drafts</h3>
+			<p class="mt-2">
+				Each year after the NFL draft the rookie draft will be held. The rookie draft will be a 5
+				round linear draft. The draft order will be determined by the reverse order of the previous
+				year's standings. The non-playoff teams will draft in the order of their standings, and the
+				playoff teams will draft in the reverse order of their playoff standings. The rookie draft
+				will be held on Sleeper and will be a slow draft, and each manager will have 8 hours to make
+				their pick. If a manager does not make their pick in time, they will be auto-drafted the
+				highest-ranked player available according to Sleeper.
+			</p>
+
+			<h3 class="mt-4 text-xl font-semibold underline">2.2 Startup Draft</h3>
+			<p class="mt-2">
+				The startup draft will be a 29-round snake draft. The draft will be held on Sleeper and will
+				be a slow draft, and each manager will have 8 hours to make their pick. If a manager does
+				not make their pick in time, they will be auto-drafted the highest-ranked player available
+				according to Sleeper. The draft order will be randomized before the start of the draft. The
+				draft order will be reversed for the second round and so on.
 			</p>
 		</section>
 
 		<!-- Section 3 -->
 		<section id="scoring" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 3: Scoring System</h2>
-			<ul class="list-inside list-disc space-y-2">
-				<li>3.1 Voting on Scoring System</li>
-			</ul>
-			<p class="mt-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+			<h2 class="mb-4 text-3xl font-bold underline">Section 3: Scoring</h2>
+			<p class="mt-2">
+				The scoring system will be a PPR TEP scoring system. The scoring system is listed on Sleeper
+				and is as follows:
 			</p>
+
+			<h3 class="mt-4 text-xl font-semibold underline">Passing</h3>
+			<ul class="list-inside list-disc space-y-2">
+				<li>Passing Yards: 1 point per 25 yards</li>
+				<li>Passing Touchdowns: 4 points</li>
+				<li>2 Point Conversions: 2 points</li>
+				<li>Interceptions: -2 points</li>
+			</ul>
+
+			<h3 class="mt-4 text-xl font-semibold underline">Rushing</h3>
+			<ul class="list-inside list-disc space-y-2">
+				<li>Rushing Yards: 1 point per 10 yards</li>
+				<li>Rushing Touchdowns: 6 points</li>
+				<li>2 Point Conversions: 2 points</li>
+				<li>Fumbles Lost: -2 points</li>
+			</ul>
+
+			<h3 class="mt-4 text-xl font-semibold underline">Receiving</h3>
+			<ul class="list-inside list-disc space-y-2">
+				<li>Receptions: 1 point</li>
+				<li>Receiving Yards: 1 point per 10 yards</li>
+				<li>Receiving Touchdowns: 6 points</li>
+				<li>2 Point Conversions: 2 points</li>
+				<li>Reception Bonus: 0.5 points for every reception by a tight end</li>
+				<li>Fumbles Lost: -2 points</li>
+			</ul>
+
+			<h3 class="mt-4 text-xl font-semibold underline">Special Teams</h3>
+			<ul class="list-inside list-disc space-y-2">
+				<li>Special Teams Touchdowns (Kick/Punt Return): 6 points</li>
+			</ul>
 		</section>
 
 		<!-- Section 4 -->
-		<section id="" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 4: Postseason</h2>
-			<ul class="list-inside list-disc space-y-2">
-				<li>4.1 Playoffs</li>
-				<li>4.2 Playoff Tiebreakers</li>
-			</ul>
-			<p class="mt-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+		<section id="Postseason" class="mb-8">
+			<h2 class="mb-4 text-3xl font-bold underline">Section 4: Postseason</h2>
+
+			<h3 class="mt-4 text-xl font-semibold underline">4.1 Playoff Format</h3>
+			<p class="mt-2">
+				The playoffs will be a 6-team playoff format. The top 2 teams will get a bye in the first
+				round. The playoffs will be held in weeks 14-17. The top 2 teams will play the lowest-seeded
+				teams in the first round. The winners of those games will play the top 2 teams in the
+				championship game. The losers of those games will play each other for 3rd place.
 			</p>
+
+			<h3 class="mt-4 text-xl font-semibold underline">4.2 Playoff Seeding</h3>
+			<p class="mt-2">
+				The playoff seeding will be determined by the order of the standings at the end of the
+				regular season. Tiebreakers will determine seeding in the case of a tie and will be
+				prioritized in the following order:
+			</p>
+			<ul class="list-inside list-disc space-y-2">
+				<li>Points For</li>
+				<li>Points Against</li>
+			</ul>
 		</section>
 
 		<!-- Section 5 -->
-		<section id="section-5" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 5: Tanking Policy</h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+		<section id="tanking" class="mb-8">
+			<h2 class="mb-4 text-3xl font-bold underline">Section 5: Tanking</h2>
+			<p class="mt-2">
+				Each manager must have a starting lineup without any out or injured players. A manager
+				should put a good faith effort towards putting out a competitive lineup. If a manager wishes
+				to tank, some good ways to do this are trading away good players to contending teams for
+				future picks, as well as leaving rookies on the taxi squad for the year.
 			</p>
 		</section>
 
 		<!-- Section 6 -->
-		<section id="section-6" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 6: Replacing Managers</h2>
+		<section id="finances" class="mb-8">
+			<h2 class="mb-4 text-3xl font-bold underline">Section 6: Finances</h2>
+
+			<h3 class="mt-4 text-xl font-semibold underline">6.1 League Fees</h3>
+			<p class="mt-2">The league buy-in will be $30 per team. The payout will be as follows:</p>
 			<ul class="list-inside list-disc space-y-2">
-				<li>6.1 Removing Managers</li>
-				<li>6.2 Replacing Managers</li>
-				<li>6.3 Replacement Incentive</li>
+				<li>$240 for 1st place (80%)</li>
+				<li>$60 for 2nd place (20%)</li>
 			</ul>
-			<p class="mt-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+			<p class="mt-2">
+				Note: This is to be paid to the managers who come in first and second in the playoffs, not
+				the regular season.
+			</p>
+
+			<h3 class="mt-4 text-xl font-semibold underline">6.2 Money Management</h3>
+			<p class="mt-2">
+				Dues will be paid to the commissioner before the start of each year, and then the
+				commissioner will pay out the winners at the end of the year.
 			</p>
 		</section>
 
 		<!-- Section 7 -->
-		<section id="section-7" class="mb-8">
-			<h2 class="mb-4 text-3xl font-bold">Section 7: League Finances</h2>
-			<ul class="list-inside list-disc space-y-2">
-				<li>7.1 League Dues</li>
-				<li>7.2 Payout</li>
-				<li>7.3 Raising Dues</li>
-			</ul>
-			<p class="mt-4">
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut
-				labore et dolore magna aliqua.
+		<section id="rules" class="mb-8">
+			<h2 class="mb-4 text-3xl font-bold underline">Section 7: League Rules</h2>
+			<p class="mt-2">
+				Fantasy football is about fun, so let's all remember that and be respectful and have fun for
+				many years to come.
 			</p>
 		</section>
 	</div>
