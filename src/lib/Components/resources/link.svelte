169ba4d1eb268310b7914bb865caed7e049dfbb3
Timothy O'Malley <tomalley24@gmail.com>
2025-04-10 22:16:18 -0500

    add resources

---
+++ src/lib/Components/resources/link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let href: string;
+	export let icon: string = ''; // Optional icon class
+	export let text: string;
+</script>
+
+<a
+	{href}
+	target="_blank"
+	rel="noopener noreferrer"
+	class="flex items-center space-x-2 rounded-lg p-2 transition hover:bg-gray-700"
+>
+	{#if icon}
+		<i class={icon}></i>
+	{/if}
+	<span class="">{text}</span>
+</a>
---
+++ src/routes/resources/+page.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import Link from '$lib/Components/resources/link.svelte';
+
+	const today = new Date();
+	const resources = [
+		{
+			href: `https://www.fantasypros.com/${today.getFullYear()}/${today.getMonth() + 1}/fantasy-football-rankings-dynasty-trade-value-chart-${today.toLocaleString('default', { month: 'long' }).toLowerCase()}-${today.getFullYear()}-update/`,
+			text: 'FantasyPros Dynasty Trade Value Chart',
+			icon: 'fas fa-chart-bar'
+		},
+		{
+			href: 'https://www.fantasypros.com/nfl/rankings/dynasty-overall.php',
+			text: 'FantasyPros Dynasty Consensus Rankings',
+			icon: 'fas fa-chart-bar'
+		},
+		{ href: 'https://www.ftnfantasy.com/', text: 'FTN Fantasy', icon: 'fas fa-chart-line' },
+		{
+			href: 'https://www.thefantasyfootballers.com/',
+			text: 'The Fantasy Footballers',
+			icon: 'fas fa-podcast'
+		},
+		{
+			href: 'https://dynastyprocess.com/calculator',
+			text: 'DynastyProcess Trade Calculator',
+			icon: 'fas fa-calculator'
+		},
+		{
+			href: 'https://dynastyleaguefootball.com/',
+			text: 'Dynasty League Football',
+			icon: 'fas fa-chart-line'
+		},
+		{
+			href: 'https://www.reddit.com/r/DynastyFF/',
+			text: 'Reddit Dynasty Fantasy Football',
+			icon: 'fas fa-file-alt'
+		},
+		{
+			href: 'https://dynasty101.com/calculator',
+			text: 'Dynasty 101 Trade Calculator',
+			icon: 'fas fa-calculator'
+		},
+		{
+			href: 'https://www.dynastynerds.com/gm/',
+			text: 'Dynasty Nerds GM',
+			icon: 'fas fa-chart-line'
+		},
+		{
+			href: 'https://www.reddit.com/r/Fantasy_Football/',
+			text: 'Reddit FF Trade Analyzer',
+			icon: 'fas fa-chart-bar'
+		},
+		{
+			href: 'https://fantasycalc.com/',
+			text: 'FantasyCalc Trade Calculator',
+			icon: 'fas fa-calculator'
+		}
+	];
+</script>
+
+<div class="mx-auto max-w-4xl p-6">
+	<h1 class="mb-6 text-center text-2xl font-bold">Helpful Dynasty Resources</h1>
+	<div class="bg-base-200 rounded-lg p-4 shadow-lg">
+		{#each resources as resource, i}
+			<Link href={resource.href} text={resource.text} icon={resource.icon} />
+			{#if i < resources.length - 1}
+				<hr class="my-2 border-gray-600" />
+			{/if}
+		{/each}
+	</div>
+	<p class="mt-4 text-center text-sm">*Some or all content is behind a paywall</p>
+</div>
