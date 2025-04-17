c04ba506dc3c9b86d0b20f463d92dd4c0173f6d5
Timothy O'Malley <tomalley24@gmail.com>
2025-04-16 21:08:11 -0500

    Update RecordsHelper.ts

---
+++ src/lib/Utilities/RecordsHelper.ts
@@ -0,0 +1,33 @@
+export class RecordsHelper {
+	// All Time team records
+	// Most points scored by a team in reg season - (top 10?)
+	//Least points scored by a team in reg season (top 10?)
+	//Most points scored by a team in playoffs
+	//Least points scored by a team in playoffs
+	//Biggest blowout in reg season
+	//Biggest blowout in playoffs
+	//Closest game in reg season
+	//Closest game in playoffs
+	//Most points scored by a team in a single game in reg season
+	//Most points scored by a team in a single game in playoffs
+	//Least points scored by a team in a single season in reg season
+	//Least points scored by a team in a single season in playoffs
+	//
+	//All Time player records
+	//Most points scored by a player in reg season
+	//Most points scored by a player in playoffs
+	//Most points scored by a player in a single game in reg season
+	//Most points scored by a player in a single game in playoffs
+	//Most poinst scored by a RB in a reg season since league inception
+	//Most points scored by a WR in a reg season since league inception
+	//Most points scored by a TE in a reg season since league inception
+	//Most points scored by a QB in a reg season since league inception
+	//Most poinst scored by an RB in a single game in reg season
+	//Most points scored by a WR in a single game in reg season
+	//Most points scored by a TE in a single game in reg season
+	//Most points scored by a QB in a single game in reg season
+	//
+	public static async GetTopTenPointsScoredByTeamInRegSeason(): Promise<any[]> {
+		return {} as any[]; // Placeholder for actual implementation
+	}
+}
